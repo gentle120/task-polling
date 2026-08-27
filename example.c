36dbcfae6bd598659b6ef7e9aca13cd6c83c0e38
yjh <1822060932@qq.com>
2026-08-27 23:11:18 +0800

    instance

---
+++ example.c
@@ -0,0 +1,44 @@
+#include "task.h"
+
+/**
+ * @example     In main function
+ */
+struct task_list task1;
+struct task_list task2;
+
+void task1_ob(struct task_list *handle, void *user_data)
+{
+    (void)handle;
+    (void)user_data;
+
+    printf("what can i say\n");
+}
+
+void task2_ob(struct task_list *handle, void *user_data)
+{
+    (void)handle;
+    (void)user_data;
+
+    printf("man\n");
+}
+
+uint32_t tick(void)
+{
+    static uint32_t cnt = 0;
+    
+    return ++cnt;
+}
+
+int main(void)
+{
+    task_init(&task1, "task1", 5, task1_ob, tick);
+    task_init(&task2, "task2", 1, task2_ob, tick);
+
+    task_member();
+
+    for(int i = 0; i < 100; i++)
+    {
+        task_start();
+    }
+
+}
